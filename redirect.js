9dcf019ebc6ddf3d2ec59d09a73685f4bdd54f3c
Dementexo <browndon962@gmail.com>
2024-05-13 16:18:50 -0500

    Endpoint change + authkey.

--- redirect.js
+++ redirect.js
@@ -4,10 +4,11 @@ form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const data = new URLSearchParams(new FormData(form));
-    const res = await fetch('https://ts-be.onrender.com/test/auth', {method:"POST", body: data});
+    const res = await fetch('https://ts-be.onrender.com/actions/wRiRLhavu8-wuch$dLBI', {method:"POST", body: data});
     const resText = await res.json();
 
     if (res.status == 200) {
+        sessionStorage.setItem("authKey", resText.AUTHCODE);
         window.location.replace(resText.URL);
     }
     else {
