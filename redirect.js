3398f86d4762b458095b0af014eab6f27d2f22b1
Dementexo <browndon962@gmail.com>
2024-05-18 15:30:17 -0500

    Added submission indicator and name/logo change.

--- redirect.js
+++ redirect.js
@@ -1,8 +1,9 @@
 const form = document.querySelector("form");
+const submitButtonHolder = document.querySelector("#submitHolder");
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
-
+    submitButtonHolder.innerHTML = "Loading...<img id=\"loadingIcon\"src=\"/assets/loadingIcon.svg\" width=\"20\"/>"
     const data = new URLSearchParams(new FormData(form));
     const res = await fetch('https://ts-be.onrender.com/actions/wRiRLhavu8-wuch$dLBI', {method:"POST", body: data});
     const resText = await res.json();
@@ -14,4 +15,5 @@ form.addEventListener("submit", async (event) => {
     else {
         alert("Error: " + resText.status);
     }
+    submitButtonHolder.innerHTML = "<input type=\"submit\" id=\"submit\" value=\"Login\">";
 })
